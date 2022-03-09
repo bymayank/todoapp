8daac48e8ba8d963685996736cdedd8e34f2f56d
bymayank <gargpitbull@gmail.com>
2022-03-09 08:35:50 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Todo from './components/todo';
-import Header from './components/header';
-import Footer from './components/footer';
+import List from "./list";
+import Todoss from "./todo";
 
 function App() {
-  return (
-    <div className='todo-app'>
-      <Header/>
-      <Todo/>
-      <Footer/>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (todo) => {
+        setTodos([...todos, {text:todo}] );
+    };
+
+    const markComplete = (index) => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = !newTodos[index].isCompleted;
+        setTodos(newTodos);
+    };
+
+    const editTitle = (index, title) => {
+        const newTodos = [...todos];
+        newTodos[index].text = title;
+        setTodos(newTodos);
+    };
+
+    return (
+      <div className="parent-container">
+          <div className="container top-container">
+              <div className="app">
+                  <div className="todoform">
+                      <List addTodo={addTodo} />
+                      <Todoss todos={todos} markComplete={markComplete} editTitle={editTitle}/>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/list.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+const TodoForm = ({addTodo}) =>{
+
+
+    const [value, setvalue] = useState('');
+
+    const handleChange = (e) => {   
+        setvalue(e.target.value);         
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();   
+
+        if (!value)             
+            return;
+        addTodo(value);
+        setvalue('');         
+    };
+
+    return(
+        <div className={"container"}>
+
+            <div className="app-title text-center">Todo app by Mayank</div>
+            <form onSubmit={handleSubmit} className={"form-group row todo-form"}>
+                <input className={"form-control col-md-8 "} style={{padding:'10px', width:'50%'}} type={"text"} placeholder={"What's up for the today!!!"} value={value} onChange={handleChange}/>
+                <input className={"form-control btn-primary col-md-4 submit-button"} type={"submit"} value={"Add Task"}/>
+            </form>
+        </div>
+    )
+};
+
+export default TodoForm;
