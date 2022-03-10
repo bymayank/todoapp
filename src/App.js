85f89f2d7de95225d75e3881b3f7eb1339120edc
bymayank <gargpitbull@gmail.com>
2022-03-10 11:50:30 +0530

    added pending task feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import List from "./list";
 import Todoss from "./todo";
 
 function App() {
     const [todos, setTodos] = useState([]);
-
+    
     const addTodo = (todo) => {
         setTodos([...todos, {text:todo}] );
     };
@@ -15,7 +15,10 @@ function App() {
         newTodos[index].isCompleted = !newTodos[index].isCompleted;
         setTodos(newTodos);
     };
-    
+    const [todoRemaining, setRemaining] = useState(0);
+    useEffect(() => { setRemaining(todos.filter(task => !task.isCompleted).length) });
+
+
 
     const remove = (i) => {
       const removed = todos.filter((t, index) => i !== index);
@@ -29,6 +32,8 @@ function App() {
           <div className="container top-container">
                   <div className="todoform">
                       <List addTodo={addTodo} />
+                    
+                      <div style={{marginBottom:'10px', color:'Black', fontSize:'20px'}} >Pending tasks #{todoRemaining}</div>
                       <Todoss todos={todos} markComplete={markComplete} remove={remove}/>
 
                   </div>
--- src/list.js
+++ src/list.js
@@ -2,8 +2,7 @@ import React, {useState} from 'react'
 
 const TodoForm = ({addTodo}) =>{
 
-
-    const [value, setvalue] = useState('');
+const [value, setvalue] = useState('');
 
     const handleChange = (e) => {   
         setvalue(e.target.value);         
@@ -22,7 +21,7 @@ const TodoForm = ({addTodo}) =>{
 
             <div className="app-title text-center">Todo app by Mayank</div>
             <form onSubmit={handleSubmit} className={"form-group row todo-form"}>
-                <input className={"form-control col-md-8 "} style={{padding:'10px', width:'50%',marginTop:'40px', marginBottom:'40px', marginRight:'10px', borderRadius:'5px'}} type={"text"} placeholder={"What's up for the today!!!"} value={value} onChange={handleChange}/>
+                <input className={"form-control col-md-8 "} style={{padding:'10px', width:'50%',marginTop:'40px', marginBottom:'20px', marginRight:'10px', borderRadius:'5px'}} type={"text"} placeholder={"What's up for the today!!!"} value={value} onChange={handleChange}/>
                 <input className={"form-control btn-primary col-md-4 submit-button"} type={"submit"} value={"Add Task"}/>
             </form>
         </div>
